daadeeca2f0ed83805370efd9c18c0adc8ccab78
PBL075 <pavelgorea75@gmail.com>
2026-04-22 21:22:05 +0300

    Adaugat RS3 cu unitati de masura

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace verificator_stage_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            string masina = "RS3";
+            int cp = 425;
+            double razgon = 3.8 ;
+
+            int putereStage1 = 50;
+
+            int totalPutere = cp + putereStage1;
+
+            Console.WriteLine("RS3 stock : " + cp + "HP");
+            Console.WriteLine(masina + " stage1 " + totalPutere + "HP" + " cu razgon de " + razgon + " sec ");
+
+        }
+    }
+}
